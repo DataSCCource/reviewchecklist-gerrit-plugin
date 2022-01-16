6e76e22aa58ae0e83dded7097c6402c891826306
DennisW <dontspamme@gmx.org>
2022-01-16 19:38:02 +0100

    Read in checklist from json file
    Change-Id: I029fcd910edf9a082be2d3ff738604fde71d3ea3

--- plugins/reviewchecklist-plugin.js
+++ plugins/reviewchecklist-plugin.js
@@ -8,11 +8,21 @@
  * @author Dennis W. (DataSCCource)
  * @since January 2022
  *
- * @usage Copy .js file to plugins folder of your Gerrit installation and wait a minute
+ * @usage Copy .js file to plugins folder of your Gerrit installation and wait a minute.
+ *        Add .review-checklist.json file to your repository with the following format:
+ *          [
+ *              {"checkpoint": "Checkpoint 1 (mandatory)", "mandatory": true},
+ *              {"checkpoint": "Checkpoint 2 (not mandatory)", "mandatory": false},
+ *              {"checkpoint": "Checkpoint 3 (not mandatory)"}
+ *          ]
+ *        'checkpoint' - string of a checklist item
+ *        'mandatory' is optional - can be true, false or omitted
+ * @requires gitiles plugin to get access to the .review-checklist.json file containing the repo specific checklist.
  */
 
 const pluginName = 'reviewchecklist-plugin';
 const pluginHook = 'reply-text';
+const fileName = '.review-checklist.json';
 
 const checklistTemplate = Polymer.html`
 <dom-module>
@@ -59,17 +69,45 @@ Gerrit.install(plugin => {
     plugin.registerCustomComponent(pluginHook, pluginName).onAttached(changeElement => {
         // "save" element-reference for later use
         hookElement = changeElement;
+        const repoName = changeElement.plugin.report.reportRepoName;
 
-        // TODO: read list from json-file
-        const cpList = [];
-        cpList.push({checkpoint: "Checkpoint 1", mandatory: true});
-        cpList.push({checkpoint: "Checkpoint 2", mandatory: false});
-        cpList.push({checkpoint: "Checkpoint 3"});
+        const filePath = `/plugins/gitiles/${repoName}/+/refs/heads/master/${fileName}?format=text`;
+        var cpList = [];
 
-        for(var i=0; i<cpList.length; i++) {
-            hookElement.checkpointsExists = true;
-            hookElement.checklistPoints.push({checkpoint: cpList[i].checkpoint, mandatory: cpList[i].mandatory, checked: false});
-            if(cpList[i].mandatory == true) hookElement.mandatoryCheckpointExists = true;
-        }
+        fetch(filePath)
+        .then(response=>response.text())
+        .then(data => {
+            if (data && data !== '') {
+                const urlArray = JSON.parse(atob(data));
+
+                urlArray.forEach(function(checkpoint) {
+                    cpList.push({checkpoint: checkpoint.checkpoint, mandatory: checkpoint.mandatory});
+                });
+                fillChecklist(cpList);
+            }
+        })
+        .catch(()=> {
+            console.log(`# Review Checklist: No existing ${fileName}, using default... `);
+
+            /* Default checklist; uncomment and fill the following lines if you want a default */
+            //cpList = [
+            //    {checkpoint: "Checkpoint 1", mandatory: true},
+            //    {checkpoint: "Checkpoint 2", mandatory: false},
+            //    {checkpoint: "Checkpoint 3"}
+            //];
+            fillChecklist(cpList);
+        });
     });
+
+    function fillChecklist(checklistArray) {
+        hookElement.checklistPoints = [];
+        hookElement.checkpointsExists = false;
+        hookElement.mandatoryCheckpointExists = false;
+
+        for(var i=0; i<checklistArray.length; i++) {
+            hookElement.checkpointsExists = true;
+            hookElement.checklistPoints.push({checkpoint: checklistArray[i].checkpoint, mandatory: checklistArray[i].mandatory, checked: false});
+            if(checklistArray[i].mandatory == true) hookElement.mandatoryCheckpointExists = true;
+        }
+    }
 });
